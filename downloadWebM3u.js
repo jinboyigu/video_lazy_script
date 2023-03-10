431c92889855461e554e44ecbf01e35c5ca2efda
jyhuang <326127285@qq.com>
2023-03-10 15:05:28 +0800

    update: 增加自动生成多个 m3u

--- downloadWebM3u.js
+++ downloadWebM3u.js
@@ -11,6 +11,7 @@ const pb = new ProgressBar(void 0, 50);
 // 可配置项
 let {
   START_EPISODE,
+  INCREASE_EPISODES,
   END_EPISODE,
   NOT_COMPLETED_EPISODE,
   EPISODE_NAME,
@@ -18,6 +19,7 @@ let {
   VIDEO_DOWNLOAD_PATH,
   AUTO_DOWNLOAD,
   EXPORT_M3U_FILE,
+  AUTO_EXPORT_M3U_NEXT,
   M3U_FILE_PATH,
   DOWNLOAD_MAX_COUNT,
 } = require('./store');
@@ -26,7 +28,7 @@ const getHtmlURL = episode => HTML_URL.replace('$episode', episode);
 const getFullPath = episode => path.resolve(VIDEO_DOWNLOAD_PATH, `${episode}.mp4`);
 
 const M3U_HEADER = '#EXTM3U';
-const m3uList = [];
+let m3uList = [];
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpgeOptionCache = [];
 
@@ -38,7 +40,7 @@ const getVideoURL = content => {
   return matched[1].replace(/\\/g, '');
 };
 
-async function getHtmlContent(url) {
+async function requestUrlContent(url) {
   let rawData = '';
   return new Promise(resolve => {
     http.get(url, {}, res => {
@@ -51,7 +53,7 @@ async function getHtmlContent(url) {
 }
 
 async function downloadWebM3u(index) {
-  return getHtmlContent(getHtmlURL(index)).then(html => {
+  return requestUrlContent(getHtmlURL(index)).then(async html => {
     const url = getVideoURL(html);
     if (!url) {
       NEED_STOP_LOOP = true;
@@ -62,7 +64,10 @@ async function downloadWebM3u(index) {
     if (EXPORT_M3U_FILE) {
       const realName = `${EPISODE_NAME}_${path.basename(name).replace(/\..*$/, '')}`;
       console.log(`${realName} m3u url: ${url}`);
-      m3uList.push(`#EXTINF:-1 ,${EPISODE_NAME}_${realName}`);
+      let duration = -1;
+      // TODO 获取真正时长
+      // await getUrlContent(url).then(content => {});
+      m3uList.push(`#EXTINF:${duration},${EPISODE_NAME}_${realName}`);
       m3uList.push(option.url);
     }
     if (!AUTO_DOWNLOAD) return;
@@ -94,16 +99,21 @@ function handleFFmpge(option) {
 }
 
 async function main() {
+  let name = [EPISODE_NAME];
   if (NOT_COMPLETED_EPISODE.length) {
-    return doNotCompleted();
-  }
-  for (let i = START_EPISODE; i <= END_EPISODE; i++) {
-    await downloadWebM3u(i);
-    if (NEED_STOP_LOOP) {
-      END_EPISODE = i;
-      break;
+    name.push(...NOT_COMPLETED_EPISODE);
+    await doNotCompleted();
+  } else {
+    let i = START_EPISODE;
+    for (; i <= END_EPISODE; i++) {
+      if (NEED_STOP_LOOP) {
+        break;
+      }
+      await downloadWebM3u(i);
     }
+    name.push(START_EPISODE, `${i - 1}`);
   }
+  return exportMu3File(name.join('_'));
 }
 
 async function doNotCompleted() {
@@ -112,11 +122,18 @@ async function doNotCompleted() {
   }
 }
 
-main().then(() => {
-  if (m3uList.length) {
-    // TODO 自动分组
-    let fileName = `${EPISODE_NAME}_${START_EPISODE}_${END_EPISODE}.m3u`;
-    fs.writeFileSync(path.resolve(M3U_FILE_PATH || __dirname, fileName), `${M3U_HEADER}\n${m3uList.join('\n')}`);
-    console.log(`${fileName} 文件更新成功`);
+async function exportMu3File(name) {
+  if (!m3uList.length) return;
+  const fileName = `${name}.m3u`;
+  fs.writeFileSync(path.resolve(M3U_FILE_PATH || __dirname, fileName), `${M3U_HEADER}\n${m3uList.join('\n')}`);
+  console.log(`${fileName} 文件更新成功`);
+  if (AUTO_EXPORT_M3U_NEXT) {
+    // 初始化参数
+    START_EPISODE += INCREASE_EPISODES + 1;
+    END_EPISODE = START_EPISODE + INCREASE_EPISODES;
+    m3uList = [];
+    return main();
   }
-});
+}
+
+main().then();
--- store.js
+++ store.js
@@ -2,8 +2,10 @@
 
 // 开始集数
 const START_EPISODE = 1;
+// 递增集数
+const INCREASE_EPISODES = 49;
 // 结束集数
-let END_EPISODE = START_EPISODE + 49;
+let END_EPISODE = START_EPISODE + INCREASE_EPISODES;
 // 存放网络问题还未下载好的或者指定集数
 const NOT_COMPLETED_EPISODE = [];
 // 剧集名称
@@ -17,12 +19,15 @@ const VIDEO_DOWNLOAD_PATH = `/xxx/${EPISODE_NAME}/`;
 const AUTO_DOWNLOAD = 1;
 // 导出 m3u 文件
 const EXPORT_M3U_FILE = !AUTO_DOWNLOAD;
+// 自动继续导出
+const AUTO_EXPORT_M3U_NEXT = true;
 // m3u 文件存放路径
 const M3U_FILE_PATH = '/xxx';
 const DOWNLOAD_MAX_COUNT = 10;
 
 module.exports = {
   START_EPISODE,
+  INCREASE_EPISODES,
   END_EPISODE,
   NOT_COMPLETED_EPISODE,
   EPISODE_NAME,
@@ -30,6 +35,7 @@ module.exports = {
   VIDEO_DOWNLOAD_PATH,
   AUTO_DOWNLOAD,
   EXPORT_M3U_FILE,
+  AUTO_EXPORT_M3U_NEXT,
   M3U_FILE_PATH,
   DOWNLOAD_MAX_COUNT,
 };
